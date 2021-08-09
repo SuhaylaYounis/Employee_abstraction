29878219c96a8f5c9fcb7b7e0b7e563f3e4477b9
SuhaylaYounis <60473969+SuhaylaYounis@users.noreply.github.com>
2021-08-09 18:57:43 +0200

    Creating an employee class using abstraction.
    Creating an employee class using abstraction.

---
+++ employee_abstract/bin/Flutter_developer.dart
@@ -0,0 +1,27 @@
+import 'Employee.dart';
+//creating a Flutter_developer class inherits abstract class  employee
+class Flutter_developer extends Employee
+{
+  //defining the data types of the variables
+  String name,position, department,language;
+  int id,experience;
+
+  //creating a constructor of flutter developer employee and sending data to the super class "Employee"
+  Flutter_developer(this.language,this.name,this.position,this.department,this.id,this.experience): super(name,position,department,id,experience );
+  //overriding the emp_position method from super class to use it in flutter developer class
+
+  void emp_Position()
+  {
+    //print the flutter developer employee's position
+    print("The flutter developer employee Position : ");
+    super.emp_Position(); //calling emp_Position method from super class
+  }
+  //overriding the salary method from super class to use it in flutter developer class
+
+  void salary()
+  {
+    //print the flutter developer employee's salary
+    print("The flutter developer employee salary: ");
+    super.salary();  //calling salary method from super class
+  }
+}
---
+++ employee_abstract/bin/employee_abstract.dart
@@ -0,0 +1,19 @@
+import 'Android_developer.dart';
+import 'Flutter_developer.dart';
+
+void main(List<String> arguments)
+{
+  //creating object of flutter developer and inserting data of the flutter developer employee
+  var flutterDeveloper = Flutter_developer('dart','dina', '','web development',12334,2);
+  //calling emp_position method which gets the position of the flutter employee
+  flutterDeveloper.emp_Position();
+  //calling salary method which gets the salary of the flutter employee
+  flutterDeveloper.salary();
+  print('*********************************************************');
+  //creating object of android developer and inserting data of the android developer employee
+  var androidDeveloper = Android_developer('java', 'ahmed', '', 'Backend developer', 35746, 1);
+  //calling emp_position method which gets the position of the android employee
+  androidDeveloper.emp_Position();
+  //calling salary method which gets the salary of the android employee
+  androidDeveloper.salary();
+}
